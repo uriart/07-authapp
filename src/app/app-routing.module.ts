301bacb0ae5e2c25518b30381141cfb92d2f26e3
uriart <loiro_8@hotmail.com>
2021-03-10 20:02:38 +0100

    Revert "first commit"
    This reverts commit 9997d80607cf25951a696380d640a9d34252bbf9.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 
 import { HomeComponent } from './components/home/home.component';
-import { MarcasComponent } from './components/marcas/marcas.component';
-import { ProgramaComponent } from './components/programa/programa.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
   { 
-    path: 'marcas', 
-    component: MarcasComponent,
-    canActivate: [AuthGuard] 
-  },
-  { 
-    path: 'programa', 
-    component: ProgramaComponent,
+    path: 'protegida', 
+    component: ProtegidaComponent,
     canActivate: [ AuthGuard ]
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { LOCALE_ID, NgModule } from '@angular/core';
-import localeEs from '@angular/common/locales/es';
-import { registerLocaleData } from '@angular/common';
-registerLocaleData(localeEs, 'es');
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProgramaComponent } from './components/programa/programa.component';
-import { MarcasComponent } from './components/marcas/marcas.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 import { AuthModule } from '@auth0/auth0-angular';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { CalculoPesoPipe } from './pipes/calculo-peso.pipe';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ProgramaComponent,
-    MarcasComponent,
-    CalculoPesoPipe
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
-    HttpClientModule,
     AuthModule.forRoot({
       domain: 'dev-r071w8pk.eu.auth0.com',
       clientId: '1oVAezqLX77ftKY3aKHX3Cr86ag2guga'
     }),
   ],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'es'
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
